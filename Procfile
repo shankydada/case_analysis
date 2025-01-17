91072f74eef7fc0d62ac4d6999b3e2b163201497
shankydada <sh.shankroy3@gmail.com>
2025-01-17 08:17:55 +0000

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn case_analysis.wsgi
--- case_analysis/urls.py
+++ case_analysis/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
     path('', views.predict, name='predict'),
+    path('admin/', admin.site.urls),
 ]
